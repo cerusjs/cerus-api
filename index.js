5e0f892ecebed554782b30db18b12ef8ab8c3ab3
JortvD <jortvandriel@hotmail.com>
2018-01-04 20:08:49 +0100

    Updated the index

--- index.js
+++ index.js
@@ -1,25 +1,23 @@
 module.exports = function() {
-	var self = {};
-
 	var package = require("./package.json");
+	var plugin = {};
+	var api;
 	
-	self.name = package["name"];
-	self.version = package["version"];
-	self.dependencies = [
+	plugin.name = package["name"];
+	plugin.version = package["version"];
+	plugin.dependencies = [
 		"cerus-router",
 		"cerus-promise",
 		"cerus-settings"
 	];
 
-	var api;
-
-	self.init_ = function(cerus) {
-		api = require("./lib/api")(cerus);
+	plugin.init_ = function(cerus) {
+		api = new (require("./lib/api"))(cerus);
 	}
 
-	self.api = function() {
+	plugin.api = function() {
 		return api;
 	}
 
-	return self;
+	return plugin;
 }
